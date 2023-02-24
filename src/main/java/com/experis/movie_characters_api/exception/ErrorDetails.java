477c7d1a8f164b5f69826b7659059607f0e98d1d
Arif Hossain <hossaininfo@ymail.com>
2023-02-24 21:56:30 +0100

    created exception package, inside created ErrorDetails class

--- src/main/java/com/experis/movie_characters_api/exception/ErrorDetails.java
+++ src/main/java/com/experis/movie_characters_api/exception/ErrorDetails.java
@@ -1,2 +1,28 @@
-package com.experis.movie_characters_api.exception;public class ErrorDetails {
+package com.experis.movie_characters_api.exception;
+
+import java.util.Date;
+
+public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public ErrorDetails(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
 }
