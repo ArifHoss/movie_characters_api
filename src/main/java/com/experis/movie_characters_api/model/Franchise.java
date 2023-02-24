05d9e1adf8ce0ae5d4f4f7565a53c798d617f72e
Arif Hossain <hossaininfo@ymail.com>
2023-02-24 11:53:19 +0100

    created Franchise entity

---
+++ src/main/java/com/experis/movie_characters_api/model/Franchise.java
@@ -0,0 +1,26 @@
+package com.experis.movie_characters_api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Franchise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "franchise_id")
+    private int id;
+    private String name;
+    private String description;
+
+    @OneToMany
+    private Set<Movie> movies;
+
+
+}
