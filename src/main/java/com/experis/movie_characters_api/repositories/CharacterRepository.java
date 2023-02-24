40c924332983a5302ebb1dea1d2973832fecedbb
Arif Hossain <hossaininfo@ymail.com>
2023-02-24 12:08:18 +0100

    created Character controller, service and repository

---
+++ src/main/java/com/experis/movie_characters_api/repositories/CharacterRepository.java
@@ -0,0 +1,7 @@
+package com.experis.movie_characters_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CharacterRepository extends JpaRepository<Character, Integer> {
+
+}
