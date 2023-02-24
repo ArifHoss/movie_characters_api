0746b9afea60f09e8f568379accaa08b2c3da503
Arif Hossain <hossaininfo@ymail.com>
2023-02-24 11:53:59 +0100

    created Character entity

---
+++ src/main/java/com/experis/movie_characters_api/model/Character.java
@@ -0,0 +1,27 @@
+package com.experis.movie_characters_api.model;
+
+import com.experis.movie_characters_api.enums.GenderType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Character {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "character_id")
+    private int id;
+    private String first_name;
+    private String last_name;
+    private String alias;
+    @Enumerated(EnumType.STRING)
+    private GenderType gender;
+    @ManyToMany
+    private Set<Movie> movies;
+
+}
