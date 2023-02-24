96026244ad5b70f3b51ff7eaae816aed00d79d70
Arif Hossain <hossaininfo@ymail.com>
2023-02-24 10:56:38 +0100

    Initial commit

---
+++ src/test/java/com/experis/movie_characters_api/MovieCharactersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experis.movie_characters_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCharactersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
