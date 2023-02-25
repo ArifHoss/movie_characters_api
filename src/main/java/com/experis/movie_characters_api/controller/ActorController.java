991a376db6465eef278d6a068126c8378d3bdce5
Arif Hossain <hossaininfo@ymail.com>
2023-02-25 12:06:45 +0100

    removed commented codes

--- src/main/java/com/experis/movie_characters_api/controller/ActorController.java
+++ src/main/java/com/experis/movie_characters_api/controller/ActorController.java
@@ -1,14 +1,12 @@
 package com.experis.movie_characters_api.controller;
 
-import com.experis.movie_characters_api.model.dto.ActorDto;
-import com.experis.movie_characters_api.model.dto.ActorMapperImpl;
+import com.experis.movie_characters_api.model.dto.*;
 import com.experis.movie_characters_api.model.entity.Actor;
 import com.experis.movie_characters_api.services.implementation.ActorServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,18 +21,11 @@ public class ActorController {
     @ResponseStatus(value = HttpStatus.OK)
     public List<ActorDto> getAllActor() {
         // Retrieves all actors from the database and mapps them to a list of 'ActorDto' objects using 'ActorMapper' class.
-        // The List is then returned a '200 OK' status code
         List<Actor> actorList = actorServiceImpl.getAllActor();
         return actorList.stream()
                 .map(actorMapper::toActorDto)
                 .collect(Collectors.toList());
     }
-
-//    @GetMapping("/{actorId}")
-//    @ResponseStatus(value = HttpStatus.OK)
-//    public Actor getActorById(@PathVariable("actorId") int id) {
-//        return actorServiceImpl.getActorById(id);
-//    }
 
     @GetMapping("/{actorId}")
     @ResponseStatus(value = HttpStatus.OK)
