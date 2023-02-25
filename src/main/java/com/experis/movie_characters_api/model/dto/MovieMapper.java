70cfc87b779a419ca0bd1d79f35d41f7f0802dd0
Arif Hossain <hossaininfo@ymail.com>
2023-02-25 12:25:38 +0100

    added two empty method to MovieMapper interface

--- src/main/java/com/experis/movie_characters_api/model/dto/MovieMapper.java
+++ src/main/java/com/experis/movie_characters_api/model/dto/MovieMapper.java
@@ -1,4 +1,9 @@
 package com.experis.movie_characters_api.model.dto;
 
+import com.experis.movie_characters_api.model.entity.Movie;
+
 public interface MovieMapper {
+    MovieDto toMovieDto(Movie movie);
+
+    Movie toMovie(MovieDto dto);
 }
