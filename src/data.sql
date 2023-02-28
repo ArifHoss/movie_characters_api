850c3725c452d4c16e70609560f9b15bf71cee44
Zakhida Ruslanova <zakhida_sul@hotmail.se>
2023-02-28 08:29:12 +0100

    Checked getAllMovie, added URL

---
+++ src/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO actor () VALUES ('');
+INSERT INTO actor () VALUES ('');
+INSERT INTO actor () VALUES ('');
+INSERT INTO actor () VALUES ('');
+INSERT INTO actor () VALUES ('');
+INSERT INTO actor () VALUES ('');
+INSERT INTO actor () VALUES ('');
+
+INSERT INTO movie (title, genre, release_year, director, picture, trailer) VALUES ();
+INSERT INTO movie () VALUES ();
+INSERT INTO movie () VALUES ();
+INSERT INTO movie () VALUES ();
+INSERT INTO movie () VALUES ();
+INSERT INTO movie () VALUES ();
+
+INSERT INTO franchise (name, description) VALUES ('Marvel Cinematic Universe', ' An American media franchise and shared universe centered on a series of superhero films produced by Marvel Studios');
+INSERT INTO franchise (name, description) VALUES ('Star Wars', 'An American epic space opera[1] multimedia franchise created by George Lucas, which began with the eponymous 1977 film[b] and quickly became a worldwide pop culture phenomenon.');
+INSERT INTO franchise (name, description) VALUES ('James Bond', 'Produced by Albert (“Cubby”) Broccoli and Harry Saltzman, it initiated one of the most successful movie franchises in history. ');
