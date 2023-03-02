703ff87d093138e8c0e83bbad6e00f3b3912c9c1
Arif Hossain <hossaininfo@ymail.com>
2023-03-02 12:55:03 +0100

    created WebUser entity added security to pom

--- src/main/java/com/experis/movie_characters_api/model/entity/WebUser.java
+++ src/main/java/com/experis/movie_characters_api/model/entity/WebUser.java
@@ -1,4 +1,19 @@
 package com.experis.movie_characters_api.model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "webuser")
+@Getter@Setter@AllArgsConstructor@NoArgsConstructor
 public class WebUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int webuser_id;
+    private String name;
+    private String email;
+    private String password;
 }
